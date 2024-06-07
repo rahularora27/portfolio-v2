249280a4dbe73525f046c07a4a2810db1ff2b233
Rahul Arora <rahularora2715@gmail.com>
2024-06-07 23:21:36 +0530

    did stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import HomePage from '../components/HomePage';
+"use client";
+import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
 
-export default function Home() {
-  return (
-    <>
-      <HomePage />
-    </>
+export default function HomePage() {
+    const words = [
+      {
+        text: "Hey!",
+      },
+      {
+        text: "I",
+      },
+      {
+        text: "am",
+      },
+      {
+        text: "Rahul.",
+        className: "text-blue-500 dark:text-yellow-300",
+      },
+    ];
+    return (
+      <div className="flex items-center justify-center h-[30rem]">
+        <TypewriterEffectSmooth words={words} />
+      </div>
   );
 }
