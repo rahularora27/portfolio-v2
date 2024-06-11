59652b254f46c4cb2728de4b4f44f9998e944e03
Rahul Arora <rahularora2715@gmail.com>
2024-06-11 23:04:32 +0530

    improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,45 @@
 // components/Navbar.tsx
 "use client";
+
 import React from "react";
+import { usePathname } from "next/navigation";
 import useThemeSwitcher from "../hooks/useThemeSwitcher";
 import { SunIcon, MoonIcon } from "./Icons";
-import Link from 'next/link';
+import Link from "next/link";
 
 export const Navbar: React.FC = () => {
+  const [mode, setMode] = useThemeSwitcher();
+  const pathname = usePathname();
+  const isAboutPage = pathname === "/about";
+  const isProjectsPage = pathname === "/projects";
 
-    const [mode, setMode] = useThemeSwitcher();
-
-    const commonClassName = "text-lg font-semibold cursor-pointer text-black hover:text-blue-500 dark:text-white dark:hover:text-yellow-300";
+  const commonClassName = "text-lg font-semibold cursor-pointer text-black hover:text-blue-500 dark:text-white dark:hover:text-yellow-300";
 
   return (
-    <nav className="p-4 fixed top-0 left-0 right-0 z-50 flex justify-between">
+    <nav className="p-4 z-50 flex justify-between">
       <ul className="flex space-x-4 items-center">
         <li>
-          <Link className={commonClassName} href="/about">
-            About
+          <Link className={commonClassName} href={isAboutPage ? "/" : "/about"}>
+            {isAboutPage ? "Home" : "About"}
           </Link>
         </li>
         <li>
-          <Link className={commonClassName} href="/projects">
-            Projects
+          <Link className={commonClassName} href={isProjectsPage ? "/" : "/projects"}>
+            {isProjectsPage ? "Home" : "Projects"}
           </Link>
         </li>
       </ul>
       <div className="flex items-center space-x-4">
-      <a
-        href="/RahulArora_Resume.pdf"
-        download="RahulArora_Resume.pdf"
-        className="bg-gray-300 hover:bg-gray-400 text-black dark:text-white font-bold py-2 px-4 rounded dark:bg-gray-800 dark:hover:bg-gray-700"
-      >
-        Resume
-      </a>
-      <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
-      {
-        mode === "dark" ?
-        <SunIcon className = {"fill-dark"} />
-        : <MoonIcon className = {"fill-dark"} />
-      }
-      </button>
+        <a
+          href="/RahulArora_Resume.pdf"
+          download="RahulArora_Resume.pdf"
+          className="bg-gray-300 hover:bg-gray-400 text-black dark:text-white font-bold py-2 px-4 rounded dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          Resume
+        </a>
+        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+          {mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />}
+        </button>
       </div>
     </nav>
   );
